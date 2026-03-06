293e3d5b73b085d7aa483116156d9eec833afb2a
Grishawork <grishakozurinworks@gmail.com>
2026-03-07 00:38:42 +0300

    Табы

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,33 @@
 import mobileNav from './modules/mobile-nav.js';
 mobileNav();
+
+document.addEventListener('DOMContentLoaded', function(){
+
+    let advantagesBox = document.querySelector('.advantages-box');
+    let tabButtons = document.querySelectorAll('.advantages-points li');
+    let tabContent = document.querySelectorAll('.advantages-info');
+
+    function openTab(index){
+
+        tabButtons.forEach(button =>{
+            button.classList.remove('active');
+        })
+
+        tabButtons[index].classList.add('active');
+
+        tabContent.forEach(content =>{
+            content.style.display = 'none';
+        })
+
+        tabContent[index].style.display = 'block';
+    }
+    
+    tabButtons.forEach((button,index)=>{
+            button.addEventListener('click',()=>{
+                openTab(index)
+            })
+        })
+        if(tabContent.length > 0){
+            openTab(0);
+        }
+})
